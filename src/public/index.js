f2ff420b927e0db44656f45746183f3ac23a0c39
myamyam <sumin030822@daum.net>
2025-11-12 16:05:47 +0900

    1112

--- src/public/index.js
+++ src/public/index.js
@@ -1,49 +1,368 @@
-const NewRoom =()=>{
-    const handleNewRoom=(event)=>{
-        event.preventDefault();
-        const name=event.target.roomname.value;
-        event.target.roomname.value="";
-        if (name.length==0) return;
-        Socket.emit("room_new", name);
-    };
+const socket = io();
 
-    return(
-        <div className="newroom">
-            <form className="newroom_form" onSubmit={handleNewRoom}>
-                <input className="newroom_input" type="text" name="roomname" placeholder="방 이름">
-                </input>
-                <button classsName="newroom_submit">방 만들기</button>
-            </form>
-        </div>
-    );
+socket.onAny((event) => {
+  console.log(`Socket event: ${event}`);
+});
+
+socket.on("error", (message) => {
+  alert(message);
+});
+
+const Header = () => {
+  return <h1 className="title">Socket Omok</h1>;
 };
 
-const RoomItem=(room)=>{
-    const handleEnterRoom=()=>{
-        Socket.emit("room_enter", room.name);
-    };
+const NewRoom = () => {
+  const handleNewRoom = (event) => {
+    event.preventDefault();
+    const name = event.target.roomname.value;
+    event.target.roomname.value = "";
+    if (name.length == 0) return;
+    socket.emit("room_new", name);
+  };
 
-    return(
-        <li key={room.name} className="room-list_item">
-            <p className="room-list_name">{room.name}</p>
-            <button className="room-list_enter" onClick={handleEnterRoom}>
-                입장하기
-            </button>
-        </li>
-    );
+  return (
+    <div className="newroom">
+      <form className="newroom__form" onSubmit={handleNewRoom}>
+        <input
+          className="newroom__input"
+          type="text"
+          name="roomname"
+          placeholder="방 이름"
+        ></input>
+        <button className="newroom__submit">방 만들기</button>
+      </form>
+    </div>
+  );
 };
 
-const RoomList=()=>{
-    const [roomList, setRoomList]=react.useState([]);
+const RoomItem = (room) => {
+  const handleEnterRoom = () => {
+    socket.emit("room_enter", room.name);
+  };
 
-    Socket.on("room_change", (list) =>{
-        setRoomList(list);
+  return (
+    <li key={room.name} className="room-list__item">
+      <p className="room-list__name">{room.name}</p>
+      <button className="room-list__enter" onClick={handleEnterRoom}>
+        입장하기
+      </button>
+    </li>
+  );
+};
+
+const RoomList = ({ roomList }) => {
+  return (
+    <div className="room-list">
+      <h3>방 목록</h3>
+      <ul className="room-list__container">{roomList.map(RoomItem)}</ul>
+    </div>
+  );
+};
+
+const WaitingRoom = () => {
+  const [roomList, setRoomList] = React.useState([]);
+  document.title = `대기실: 참가 가능한 방 ${roomList.length}개`;
+  React.useEffect(() => {
+    socket.on("room_list", (list) => {
+      console.log(list);
+      setRoomList(list);
     });
-    
-    return(
-        <div className="room-list">
-            <h3>방 목록</h3>
-            <ul className="room-list_container">{roomList.map(Roomitem)}</ul>
-        </div>
-    );
+    socket.emit("room_list");
+
+    return () => {
+      socket.off("room_list");
+    };
+  }, []);
+
+  return (
+    <div className="waiting-room">
+      <NewRoom />
+      <RoomList roomList={roomList} />
+    </div>
+  );
 };
+
+////////////////////////////////////////////////////////////////////////
+const BOARD_OFFSET = 3.62; //%
+const BOARD_SPACE = 5.14; //%
+//크기: 5%
+//칸: 5.14%
+//공백: 3.62%
+const stone = ({ type, x, y }) => {
+  //console.log(`${white} (${x},${y})`);
+  let material = "";
+  type.forEach((m) => {
+    switch (m) {
+      case "black":
+        material += " omokboard__stone--black";
+        break;
+      case "white":
+        material += " omokboard__stone--white";
+        break;
+      case "hint":
+        material += " omokboard__stone--hint";
+        break;
+      case "prev":
+        material += " omokboard__stone--prev";
+        break;
+    }
+  });
+
+  return (
+    <div
+      className={`omokboard__stone ${material}`}
+      key={`${x}${y}`}
+      style={{
+        left: `${x * BOARD_SPACE + BOARD_OFFSET}%`,
+        top: `${y * BOARD_SPACE + BOARD_OFFSET}%`,
+      }}
+    ></div>
+  );
+};
+
+const MemoriedStone = React.memo(stone);
+
+const OmokBoard = ({ takes }) => {
+  const [inBoard, setInBoard] = React.useState(false);
+  const [myTurn, setMyTurn] = React.useState(false);
+  const [coord, setCoord] = React.useState({});
+  const [isGameEnd, setGameEnd] = React.useState(false);
+
+  React.useState(() => {
+    socket.on("player_select", () => {
+      setMyTurn(true);
+    });
+
+    socket.on("player_change", () => {
+      setMyTurn(false);
+    });
+  }, []);
+
+  const handleBoardEnter = () => {
+    setInBoard(true);
+  };
+
+  const handleBoardLeave = () => {
+    setInBoard(false);
+  };
+
+  const handleBoardMove = (coord) => {
+    //이미 돌이 존재하면 건너뜀
+    if (takes.find((c) => c.x === coord.x && c.y === coord.y) === undefined) {
+      setCoord(coord);
+    }
+  };
+
+  const handleBoardSelect = () => {
+    setMyTurn(false);
+    console.log(`Select [${coord.x},${coord.y}]`);
+    socket.emit("player_selected", coord);
+  };
+
+  return (
+    <div className="omokboard">
+      {myTurn ? (
+        <CoordSelectArea
+          onBoardEnter={handleBoardEnter}
+          onBoardMove={handleBoardMove}
+          onBoardLeave={handleBoardLeave}
+          onBoardSelect={handleBoardSelect}
+        />
+      ) : null}
+      {takes.map((takes, index) => (
+        <MemoriedStone
+          type={[index % 2 === 0 ? "black" : "white"]}
+          x={takes.x}
+          y={takes.y}
+        />
+      ))}
+      {takes.length > 0 ? (
+        <MemoriedStone
+          type={["prev"]}
+          x={takes[takes.length - 1].x}
+          y={takes[takes.length - 1].y}
+        />
+      ) : null}
+      {myTurn && inBoard ? (
+        <MemoriedStone
+          type={[takes.length % 2 == 0 ? "black" : "white", "hint"]}
+          x={coord.x}
+          y={coord.y}
+        />
+      ) : null}
+    </div>
+  );
+};
+
+const GamePanel = ({ roomname, blackPlayer, whitePlayer }) => {
+  const [message, setMessage] = React.useState([]);
+
+  React.useEffect(() => {
+    socket.on("message", (msg) => {
+      setMessage((value) => [...value, msg]);
+    });
+  }, []);
+
+  const Player = ({ name, onClick }) => {
+    return (
+      <div className="game-panel__playerinfo">
+        {name !== "" ? (
+          <p className="game-panel__playername">{name}</p>
+        ) : (
+          <button className="game-panel__playerselect" onClick={onClick}>
+            참가
+          </button>
+        )}
+      </div>
+    );
+  };
+
+  const MessageLine = (msg) => {
+    return (
+      <>
+        {msg}
+        <br />
+      </>
+    );
+  };
+
+  const blackPlayerCallback = () => {
+    socket.emit("player_change", "black");
+  };
+
+  const whitePlayerCallback = () => {
+    socket.emit("player_change", "white");
+  };
+
+  return (
+    <div className="game-panel">
+      <div className="game-panel__main">
+        <h3 className="game-panel__title">{roomname}</h3>
+        <div className="game-panel__players">
+          <div className="game-panel__player">
+            <h4 className="game-panel__playercolor game-panel__playercolor--black">
+              Black
+            </h4>
+            <Player name={blackPlayer} onClick={blackPlayerCallback} />
+          </div>
+          <div className="game-panel__player">
+            <h4 className="game-panel__playercolor game-panel__playercolor--white">
+              White
+            </h4>
+            <Player name={whitePlayer} onClick={whitePlayerCallback} />
+          </div>
+        </div>
+        <div className="game-panel__message">
+          <p>{message.map(MessageLine)}</p>
+        </div>
+      </div>
+      <div className="game-panel__buttons">
+        <button
+          className="game-panel__button"
+          onClick={() => socket.emit("player_change", "spectator")}
+        >
+          관전하기
+        </button>
+        <button
+          className="game-panel__button"
+          onClick={() => {
+            socket.emit("room_leave");
+          }}
+        >
+          방 나가기
+        </button>
+      </div>
+    </div>
+  );
+};
+
+const GamingRoom = ({ publicRoom }) => {
+  const [roomName, setRoomName] = React.useState(publicRoom.name);
+  const [blackPlayer, setBlackPlayer] = React.useState(publicRoom.blackPlayer);
+  const [whitePlayer, setWhitePlayer] = React.useState(publicRoom.whitePlayer);
+  const [takes, setTakes] = React.useState(publicRoom.takes);
+
+  const [winner, setWinner] = React.useState("");
+
+  console.log(publicRoom);
+  document.title = `공개방: ${roomName}`;
+
+  const onGameEnd = () => {
+    setWinner("");
+  };
+
+  const GameEndScreen = ({ winner }) => {
+    const text = `${winner === "black" ? "흑돌" : "백돌"} 승리!`;
+    return (
+      <div className="endscreen">
+        <div className="endscreen__main">
+          <h3 className="endscreen__text">{text}</h3>
+          <button className="endscreen__button" onClick={onGameEnd}>
+            확인
+          </button>
+        </div>
+      </div>
+    );
+  };
+
+  React.useEffect(() => {
+    socket.on("player_change", ({ blackPlayer, whitePlayer }) => {
+      setBlackPlayer(blackPlayer);
+      setWhitePlayer(whitePlayer);
+      if (blackPlayer !== "" && whitePlayer !== "") {
+        setTakes([]);
+      }
+    });
+
+    socket.on("player_selected", (coord) => {
+      console.log(`player_selected [${coord.x},${coord.y}]`);
+      console.log(takes);
+      setTakes((t) => [...t, coord]);
+    });
+
+    socket.on("game_end", (winner) => {
+      setWinner(winner);
+    });
+  }, []);
+
+  return (
+    <div className="gaming-room">
+      <OmokBoard takes={takes} />
+      <GamePanel
+        roomname={roomName}
+        blackPlayer={blackPlayer}
+        whitePlayer={whitePlayer}
+      />
+      {winner !== "" ? <GameEndScreen winner={winner} /> : null}
+    </div>
+  );
+};
+
+const App = () => {
+  const [publicRoom, setPublicRoom] = React.useState({});
+
+  React.useEffect(() => {
+    socket.on("room_enter", (room) => {
+      console.log(`Enter room ${room.name}`);
+      setPublicRoom(room);
+    });
+
+    socket.on("room_leave", () => {
+      setPublicRoom({});
+    });
+  }, []);
+
+  return (
+    <>
+      <Header />
+      {publicRoom.name === undefined ? (
+        <WaitingRoom />
+      ) : (
+        <GamingRoom publicRoom={publicRoom} />
+      )}
+    </>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
--- src/server.js
+++ src/server.js
@@ -1,16 +1,163 @@
-import express from "express"
-const app=express();
-const port=3000;
+import express from "express";
+import path from "path";
+import { Server } from "socket.io";
+import http from "http";
 
-wsServer.on("connection", (socket)=>{
-   socket.onAny((event)=>{
-    console.log('Socket event: ${event}');
-   });
-   
-   socket.on("room_new", (name)=>{
-    name=name.trim();
-    console.log('Socket ${socket.id} is creating room ${name}.');
+const __dirname = path.resolve() + "/src";
+const __viewroot = path.join(__dirname, "/public/views");
 
+const app = express();
+const port = 3000;
+
+app.use("/public", express.static(__dirname + "/public"));
+app.get("/", (req, res) => {
+  res.sendFile("index.html", { root: __viewroot });
+});
+
+const httpServer = http.createServer(app);
+const wsServer = new Server(httpServer);
+
+///////////////////////////////////////////////////////////
+
+let publicRoom = [];
+
+function getJoinedRoomName(socket) {
+  return Array.from(socket.rooms)[1];
+}
+
+function getPublicRoom(name) {
+  return publicRoom.find((room) => room.name == name);
+}
+
+function findSocketByID(id) {
+  return wsServer.sockets.sockets.get(id);
+}
+
+//이름이 name인 방에 속한 Socket 개수 반환
+function countRoom(name) {
+  return wsServer.sockets.adapter.rooms.get(name).size;
+}
+
+//중복된 이름의 방이 존재할 경우 false, 없을 경우 true
+function checkDuplicateRoomName(name) {
+  if (wsServer.sockets.adapter.rooms.get(name)) {
+    return false;
+  } else {
+    return true;
+  }
+}
+
+function emitPlayerChange(room) {
+  wsServer.in(room.name).emit("player_change", {
+    blackPlayer: room.blackPlayer,
+    whitePlayer: room.whitePlayer,
+  });
+
+  if (room.blackPlayer !== "" && room.whitePlayer !== "") {
+    room.takes = [];
+    findSocketByID(room.blackPlayer).emit("player_select");
+  }
+}
+
+function enterRoom(socket, name) {
+  const room = getPublicRoom(name);
+  console.log(`Socket ${socket.id} is entering room ${name}.`);
+
+  if (room === undefined) {
+    socket.emit("error", "정상적인 방이 아닙니다.");
+    return;
+  }
+
+  socket.join(name);
+  socket.emit("room_enter", room);
+  wsServer.to(name).emit("message", `${socket.id} 님이 입장하셨습니다.`);
+}
+
+function leaveRoom(socket) {
+  const name = getJoinedRoomName(socket);
+
+  console.log(`Socket ${socket.id} is leaving room ${name}.`);
+
+  if (name != undefined) {
+    //현재 Disconnect 하는 Socket이 해당 방의 마지막 소켓일 경우 방 제거
+    if (countRoom(name) == 1) {
+      console.log(`Remove room ${name}`);
+      publicRoom = publicRoom.filter((value) => value.name != name);
+      wsServer.sockets.emit("room_list", publicRoom);
+    } else {
+      const room = getPublicRoom(name);
+      if (room.blackPlayer === socket.id) {
+        room.blackPlayer = "";
+        emitPlayerChange(room);
+      } else if (room.whitePlayer === socket.id) {
+        room.whitePlayer = "";
+        emitPlayerChange(room);
+      }
+
+      wsServer.to(name).emit("message", `${socket.id} 님이 퇴장하셨습니다.`);
+    }
+    socket.leave(name);
+  }
+}
+
+//오목 완성 판별
+function checkOmokCompleted(coord, takes) {
+  //(0, 1), (1, 1), (1, 0), (1, -1)
+  const offset = [
+    { x: 1, y: 0 }, //가로
+    { x: 1, y: 1 }, //대각선1
+    { x: 0, y: 1 }, //세로
+    { x: -1, y: 1 }, //대각선2
+  ];
+
+  return offset.some((dir) => {
+    let streak = 1;
+    const type = (takes.length - 1) % 2;
+
+    //정방향
+    for (
+      let x = coord.x + dir.x, y = coord.y + dir.y;
+      x > 0 && x < 19 && y > 0 && y < 19;
+      x += dir.x, y += dir.y
+    ) {
+      if (takes.some((t, index) => t.x == x && t.y == y && index % 2 == type))
+        streak++;
+      else break;
+    }
+
+    //반대방향
+    for (
+      let x = coord.x - dir.x, y = coord.y - dir.y;
+      x > 0 && x < 19 && y > 0 && y < 19;
+      x -= dir.x, y -= dir.y
+    ) {
+      if (takes.some((t, index) => t.x == x && t.y == y && index % 2 == type))
+        streak++;
+      else break;
+    }
+
+    if (streak === 5) {
+      return true;
+    }
+  });
+}
+
+wsServer.on("connection", (socket) => {
+  socket.onAny((event) => {
+    console.log(`Socket event: ${event}`);
+  });
+
+  //방 목록 반환
+  socket.on("room_list", () => {
+    socket.emit("room_list", publicRoom);
+  });
+
+  //방 만들기
+  socket.on("room_new", (name) => {
+    name = name.trim();
+    console.log(`Socket ${socket.id} is creating room ${name}.`);
+
+    //Socket은 ID와 같은 Room을 Default로 갖고 있음
     if (socket.rooms.size > 1) {
       console.log(`socket ${socket.id} is already in room.`);
       console.log(socket.rooms);
@@ -18,36 +165,147 @@ wsServer.on("connection", (socket)=>{
       return;
     }
 
+    //동일한 방이 존재할 경우
     if (!checkDuplicateRoomName(name)) {
       console.log(`Room name ${name} already exists.`);
       socket.emit("error", "동일한 방이 이미 존재합니다.");
       return;
     }
 
-    const roomInfo={
+    const roomInfo = {
       name: "room",
-      blackPlayer:"",
-      whitePlayer:"",
-      takes:[],
+      blackPlayer: "",
+      whitePlayer: "",
+      takes: [],
     };
 
     roomInfo.name = name;
-    roomInfo.blackPlayer = socket.id;
 
     publicRoom.push(roomInfo);
-    wsServer.sockets.emit("room_change", publicRoom);
+    wsServer.sockets.emit("room_list", publicRoom);
 
-    socket.join(name);
-    socket.emit("room_enter", name);
+    enterRoom(socket, name);
+  });
 
-    console.log(publicRoom);
+  //기존 방 참가
+  socket.on("room_enter", (name) => {
+    if (socket.rooms.size > 1) {
+      console.log(`socket ${socket.id} is already in room.`);
+      console.log(socket.rooms);
+      socket.emit("error", "이미 다른 방에 참가중입니다.");
+      return;
+    }
+
+    enterRoom(socket, name);
+  });
+
+  socket.on("room_leave", () => {
+    leaveRoom(socket);
+    socket.emit("room_leave");
+  });
+
+  socket.on("player_change", (color) => {
+    const roomName = getJoinedRoomName(socket);
+    const room = getPublicRoom(roomName);
+
+    if (color === "black") {
+      if (room.blackPlayer !== "") {
+        socket.emit("error", "다른 플레이어가 참가중입니다.");
+        return;
+      } else {
+        if (room.whitePlayer === socket.id) room.whitePlayer = "";
+        room.blackPlayer = socket.id;
+      }
+    } else if (color === "white") {
+      if (room.whitePlayer !== "") {
+        socket.emit("error", "다른 플레이어가 참가중입니다.");
+        return;
+      } else {
+        if (room.blackPlayer === socket.id) room.blackPlayer = "";
+        room.whitePlayer = socket.id;
+      }
+    } else if (color === "spectator") {
+      if (room.blackPlayer === socket.id) {
+        room.blackPlayer = "";
+      } else if (room.whitePlayer === socket.id) {
+        room.whitePlayer = "";
+      } else {
+        return;
+      }
+    }
+
+    emitPlayerChange(room);
+  });
+
+  socket.on("player_selected", (coord) => {
+    const name = getJoinedRoomName(socket);
+    const room = getPublicRoom(name);
+
+    if (room === undefined) {
+      console.log(`Room ${name} is not existing.`);
+      return;
+    }
+
+    const isBlackTurn = room.takes.length % 2 == 0;
+
+    if (isBlackTurn) {
+      //흑돌
+      if (room.blackPlayer !== socket.id) {
+        socket.emit("error", "흑돌 플레이어가 아닙니다.");
+        return;
+      }
+    } else {
+      //백돌
+      if (room.whitePlayer !== socket.id) {
+        socket.emit("error", "백돌 플레이어가 아닙니다.");
+        return;
+      }
+    }
+
+    if (
+      findSocketByID(room.blackPlayer) === undefined ||
+      findSocketByID(room.whitePlayer) === undefined
+    ) {
+      socket.emit("error", "상대가 존재하지 않습니다.");
+      return;
+    }
+
+    if (
+      room.takes.find((c) => c.x === coord.x && c.y === coord.y) !== undefined
+    ) {
+      socket.emit("error", "이미 다른 돌이 위치하고 있습니다.");
+      socket.emit("player_select");
+      return;
+    }
+
+    room.takes.push(coord);
+    wsServer.in(name).emit("player_selected", coord);
+
+    if (checkOmokCompleted(coord, room.takes)) {
+      console.log("Omok completed!");
+      wsServer.in(name).emit("game_end", isBlackTurn ? "black" : "white");
+      wsServer.in(name).emit("message", `${socket.id}님이 승리하셨습니다!`);
+      room.blackPlayer = "";
+      room.whitePlayer = "";
+      emitPlayerChange(room);
+      return;
+    }
+
+    if (isBlackTurn) {
+      findSocketByID(room.whitePlayer).emit("player_select");
+    } else {
+      findSocketByID(room.blackPlayer).emit("player_select");
+    }
+  });
+
+  socket.on("player_ready", () => {});
+
+  socket.on("disconnecting", () => {
+    console.log(`Socket ${socket.id} is disconnecting.`);
+    leaveRoom(socket);
+  });
 });
-});
 
-app.get("/", (req, res)=>{
-    res.send("Hello World!");
-});
-
-app.listen(port, ()=>{
-    console.log('Example app listening on port ${port}');
+httpServer.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
 });
